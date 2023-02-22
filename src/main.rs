10963f72e59606e595d2b3d48ed12260674ee6d8
Shai Caspin <cashai99@gmail.com>
2023-02-22 17:09:44 -0500

    some things that dont work

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,60 @@
 use warp::Filter;
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+
+struct Server<T> {
+    message_storage: Arc<Mutex<HashMap<String, Vec<T>>>>,
+    clients: Arc<Mutex<HashMap<String, String>>>,
+    // notifier: Arc<Mutex<()>>,
+    // new_clients: Arc<Mutex<()>>,
+    // closing_clients: Arc<Mutex<()>>,
+}
+
+impl<T> Server<T>{
+    fn new() -> Server<T> {
+        let server = Server {
+            message_storage: Arc::new(Mutex::new(HashMap::new())),
+            clients: Arc::new(Mutex::new(HashMap::new())),
+            // notifier: Arc::new(Mutex::new(Vec::new())),
+            // new_clients: Arc::new(Mutex::new(Vec::new())),
+            // closing_clients: Arc::new(Mutex::new(Vec::new())),
+        };
+        server
+    }
+}
 
 mod models {
     use serde_derive::{Deserialize, Serialize};
-    use std::sync::Arc;
-    use tokio::sync::Mutex;
+    use std::sync::{Arc, Mutex};
 
-    // TODO: make persistent
-    pub type Db = Arc<Mutex<Vec<Todo>>>;
+    pub type Db = Arc<Mutex<Vec<Mailbox>>>;
 
     pub fn blank_db() -> Db {
         Arc::new(Mutex::new(Vec::new()))
     }
 
     #[derive(Serialize, Deserialize, Debug)]
-    struct IncomingMessage {
+    pub struct IncomingMessage {
         device_id: String,
         payload: String,
     }
 
     #[derive(Serialize, Deserialize, Debug)]
-    struct OutgoingMessage {
+    pub struct OutgoingMessage {
         sender: String,
         payload: String,
         seq_id: u64,
     }
 
-    struct Message {
+    pub struct Message {
         to: String,
         outgoing: OutgoingMessage,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Mailbox {
+        owner: String,
+        messages: Vec<OutgoingMessage>
     }
 }
 
@@ -38,7 +65,7 @@ mod filters {
 
     // handle /message
     pub fn message(db:Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        message_post(db);
+        message_post(db)
     }
 
     // POST /message
@@ -56,39 +83,47 @@ mod filters {
         db: Db,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         messages_get(db.clone())
-            .or(messages_delete(db.clone())
+            .or(messages_delete(db.clone()))
     }
 
     // GET /self/messages
-    pub fn messages_get() -> Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    pub fn messages_get(
+        db: Db,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         unimplemented!();
     }
 
     // DELETE /self/messages
-    pub fn messages_delete() -> Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    pub fn messages_delete(
+        db: Db,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         unimplemented!();
+    }
+
+    fn with_db(db: Db) -> impl Filter<Extract = (Db,), Error = std::convert::Infallible> + Clone {
+        warp::any().map(move || db.clone())
     }
 
 }
 
 mod handlers {
-    fn post_message(&self, rw: &str, req: &str) {
+    pub fn post_message(rw: &str, req: &str) {
         unimplemented!();
     }
 
-    fn get_messages(&self, rw: &str, req: &str) {
+    pub fn get_messages(rw: &str, req: &str) {
         unimplemented!();
     }
 
-    fn delete_messages(&self, rw: &str, req: &str) {
+    pub fn delete_messages(rw: &str, req: &str) {
         unimplemented!();
     }
 
-    fn get_one_time_key(&self, rw: &str, req: &str) {
+    pub fn get_one_time_key(rw: &str, req: &str) {
         unimplemented!();
     }
 
-    fn add_one_time_keys(&self, rw: &str, req: &str) {
+    pub fn add_one_time_keys(rw: &str, req: &str) {
         unimplemented!();
     }
 
