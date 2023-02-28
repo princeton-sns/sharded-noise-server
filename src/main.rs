b4fb1584820a2db99fc29c7acbcfe8e0a34f0987
Shai Caspin <cashai99@gmail.com>
2023-02-28 17:46:08 -0500

    attempt to use pickle db

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,107 @@
-use warp::Filter;
-use std::sync::{Arc, Mutex};
-use std::collections::HashMap;
+use serde_derive::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use pickledb::{PickleDb, PickleDbDumpPolicy, SerializationMethod};
+use warp::{Filter, Rejection, Reply};
+// use warp::header::{HeaderMap, HeaderValue, AUTHORIZATION};
 
-struct Server<T> {
-    message_storage: Arc<Mutex<HashMap<String, Vec<T>>>>,
-    clients: Arc<Mutex<HashMap<String, String>>>,
-    // notifier: Arc<Mutex<()>>,
-    // new_clients: Arc<Mutex<()>>,
-    // closing_clients: Arc<Mutex<()>>,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Batch{
+    messages: Vec<IncomingMessage>,
 }
 
-impl<T> Server<T>{
-    fn new() -> Server<T> {
-        let server = Server {
-            message_storage: Arc::new(Mutex::new(HashMap::new())),
-            clients: Arc::new(Mutex::new(HashMap::new())),
-            // notifier: Arc::new(Mutex::new(Vec::new())),
-            // new_clients: Arc::new(Mutex::new(Vec::new())),
-            // closing_clients: Arc::new(Mutex::new(Vec::new())),
+#[derive(Serialize, Deserialize, Debug)]
+pub struct IncomingMessage {
+    device_id: String,
+    payload: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct OutgoingMessage {
+    sender: String,
+    payload: String,
+    seq_id: u64,
+}
+
+pub struct Message {
+    to: String,
+    outgoing: OutgoingMessage,
+}
+
+struct Store {
+    db: PickleDb,
+}
+
+impl Store {
+    fn new(db_path: &str) -> Self {
+        let db = PickleDb::new(
+            db_path,
+            PickleDbDumpPolicy::AutoDump,
+            SerializationMethod::Json,
+        );
+        Store { db }
+    }
+
+    fn add_message(&mut self, device_id: String, message: &OutgoingMessage) -> Result<(), pickledb::error::Error> {
+        // add locks on this (or txn in pickle?)
+        let res;
+        if !self.db.lexists(&device_id) {
+            res = self.db.lcreate(&device_id).unwrap().ladd(message);
+            Ok(())
+        } else {
+            res = self.db.ladd(&device_id, message);
+            Ok(())
+        }
+    }
+}
+
+async fn post_message(
+    batch: Batch,
+    store: Store,
+) -> Result<impl Reply, Rejection> {
+
+    //get sender id
+    let sender_id = warp::header::<String>("authorization");
+
+    //unbatch and puth messages
+    // let batch = warp::body()::<Batch>("batch");
+    for message in batch.messages.iter() {
+        let mut m = OutgoingMessage{
+            sender: sender_id,
+            payload: message.payload,
+            seq_id: 0, //FIXME: impl this
         };
-        server
+        store.add_message(message.device_id, &m);
     }
+    Ok(format!("Message added from : {:?}", sender_id))
 }
-
-mod models {
-    use serde_derive::{Deserialize, Serialize};
-    use std::sync::{Arc, Mutex};
-
-    pub type Db = Arc<Mutex<Vec<Mailbox>>>;
-
-    pub fn blank_db() -> Db {
-        Arc::new(Mutex::new(Vec::new()))
-    }
-
-    #[derive(Serialize, Deserialize, Debug)]
-    pub struct IncomingMessage {
-        device_id: String,
-        payload: String,
-    }
-
-    #[derive(Serialize, Deserialize, Debug)]
-    pub struct OutgoingMessage {
-        sender: String,
-        payload: String,
-        seq_id: u64,
-    }
-
-    pub struct Message {
-        to: String,
-        outgoing: OutgoingMessage,
-    }
-
-    #[derive(Serialize, Deserialize, Debug)]
-    pub struct Mailbox {
-        owner: String,
-        messages: Vec<OutgoingMessage>
-    }
-}
-
-mod filters {
-    use super::handlers;
-    use super::models::{Db, IncomingMessage, OutgoingMessage, Message};
-    use warp::Filter;
-
-    // handle /message
-    pub fn message(db:Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        message_post(db)
-    }
-
-    // POST /message
-    pub fn message_post(
-        db: Db,
-    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        warp::path!("message")
-            .and(warp::post())
-            .and(with_db(db))
-            .and_then(handlers::post_message)
-    }
-
-    // handle /self/messages
-    pub fn messages(
-        db: Db,
-    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        messages_get(db.clone())
-            .or(messages_delete(db.clone()))
-    }
-
-    // GET /self/messages
-    pub fn messages_get(
-        db: Db,
-    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        unimplemented!();
-    }
-
-    // DELETE /self/messages
-    pub fn messages_delete(
-        db: Db,
-    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        unimplemented!();
-    }
-
-    fn with_db(db: Db) -> impl Filter<Extract = (Db,), Error = std::convert::Infallible> + Clone {
-        warp::any().map(move || db.clone())
-    }
-
-}
-
-mod handlers {
-    pub fn post_message(rw: &str, req: &str) {
-        unimplemented!();
-    }
-
-    pub fn get_messages(rw: &str, req: &str) {
-        unimplemented!();
-    }
-
-    pub fn delete_messages(rw: &str, req: &str) {
-        unimplemented!();
-    }
-
-    pub fn get_one_time_key(rw: &str, req: &str) {
-        unimplemented!();
-    }
-
-    pub fn add_one_time_keys(rw: &str, req: &str) {
-        unimplemented!();
-    }
-
-}
-
 
 #[tokio::main]
 async fn main() {
-    let db = models::blank_db();
-    let routes = filters::message(db).with(filters::messages(db));
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    let mut store = Store::new("noise.db");
+
+    // Define routes for handling messages
+    let post_message_route = warp::path("message")
+        .and(warp::post())
+        .and(warp::header::headers_cloned())
+        .and(warp::body::json())
+        .and_then(post_message);
+
+    // let get_messages_route = warp::path!("self/messages")
+    //     .and(warp::get())
+    //     .and_then(get_messages);
+
+    // let delete_messages_route = warp::path!("self/messages")
+    //     .and(warp::delete())
+    //     .and_then(delete_messages);
+
+    // let routes = post_message_route.and(get_messages_route).and(delete_messages_route);
+
+    warp::serve(post_message_route).run(([127, 0, 0, 1], 3030)).await
+}
+
+#[tokio::test]
+async fn test_message_post() {
+    unimplemented!();
 }
