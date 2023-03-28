9c97b41c6659ac59ff080b1c95ed03f6ab5fab57
Shai Caspin <cashai99@gmail.com>
2023-03-28 17:11:39 -0400

    [WiP] more robut testing

--- tests/goose/src/main.rs
+++ tests/goose/src/main.rs
@@ -2,25 +2,47 @@ use goose::prelude::*;
 use serde_json::json;
 use serde::{Serialize, Deserialize};
 use reqwest::header::{HeaderMap,HeaderValue};
+use random_string::generate;
+use rand::seq::SliceRandom;
+
+static Users : u8 = 1000;
+static UserIds : Vec<String> = vec![None,  Users];
+
+fn generate_pair() -> (char, char) {
+
+    let senderId = UserIds.choose(&mut rand::thread_rng());
+    let recId = '0';
+    while senderId != recId {
+        recId = UserIds.choose(&mut rand::thread_rng());
+    }
+
+    (senderId, recId)
+}
 
 #[tokio::main]
 async fn main() -> Result<(), GooseError> {
+
+    let charset = "1234567890abcdefghijklmnopqrstuvwxyz";
+    for i in 0..Users {
+        UserIds[i as usize ] = generate(2, charset);
+    }
+
     let g = GooseAttack::initialize()?;
 
-    g.register_scenario(scenario!("PostTest").register_transaction(transaction!(post_message)))
-        .execute()
-        .await?;
+    g.register_scenario(scenario!("PostTest").register_transaction(transaction!(post_message))).execute().await?;
 
     Ok(())
 }
 
 async fn post_message(user: &mut GooseUser) -> TransactionResult {
 
+    let (sender, recipient) = generate_pair();
+
     // Build a custom HeaderMap to include with all requests made by this client.
     let mut headers = HeaderMap::new();
     headers.insert(
         "Authorization",
-        HeaderValue::from_str("Bearer a").unwrap(),
+        HeaderValue::from_str("Bearer ".concat(sender)).unwrap(),
     );
     headers.insert("Content-Type", HeaderValue::from_str("application/json").unwrap());
 
@@ -31,9 +53,10 @@ async fn post_message(user: &mut GooseUser) -> TransactionResult {
         {
             "batch": 
             [
-            {"deviceId": "y",
-            "payload": {"cType":0, "ciphertext": "hello"}}
-
+            {"deviceId": sender,
+            "payload": {"cType":0, "ciphertext": "hello"}},
+            { "deviceId": recipient,
+            "payload": {"cType":0, "ciphertext": "goodbye"}},
             ]
         });
     
