a6510602b4f3b485cfa49fee22b713bd8181d8d9
Shai Caspin <cashai99@gmail.com>
2023-03-29 11:52:45 -0400

    clean up tests

--- tests/goose/src/main.rs
+++ tests/goose/src/main.rs
@@ -7,6 +7,7 @@
 use goose::prelude::*;
 use rand::seq::SliceRandom;
 use reqwest::header::{HeaderMap, HeaderValue};
+use serde::Serialize;
 use serde_json::json;
 
 static LETTERS: [&str; 26] = [
@@ -20,19 +21,30 @@ async fn main() -> Result<(), GooseError> {
     let g = GooseAttack::initialize()?;
 
     g.register_scenario(
-        scenario!("PostToOneUserAndSelf").register_transaction(transaction!(post_message)),
+        scenario!("PostToOneUserAndSelf")
+            .register_transaction(transaction!(set_username).set_name("generate username"))
+            .register_transaction(transaction!(post_message).set_name("post request")),
     )
     .register_scenario(
-        scenario!("DeleteOneMailbox").register_transaction(transaction!(delete_mailbox_content)),
+        scenario!("DeleteOneMailbox")
+            .register_transaction(transaction!(set_username).set_name("generate username"))
+            .register_transaction(transaction!(delete_mailbox_content).set_name("delete mailbox")),
     )
     .register_scenario(
-        scenario!("DeletePostDelete").register_transaction(transaction!(delete_post_delete)),
+        scenario!("DeletePostDelete")
+            .register_transaction(transaction!(set_username).set_name("generate username"))
+            .register_transaction(transaction!(delete_mailbox_content).set_name("delete mailbox"))
+            .register_transaction(transaction!(post_message).set_name("post request"))
+            .register_transaction(transaction!(delete_mailbox_content).set_name("delete mailbox")),
     )
     .execute()
     .await?;
 
     Ok(())
 }
+
+#[derive(Serialize)]
+struct Username(String);
 
 fn generate_username() -> String {
     let Some(letter) = LETTERS.choose(&mut rand::thread_rng()) else {todo!()};
@@ -41,22 +53,18 @@ fn generate_username() -> String {
     letter.to_string() + &number.to_string()
 }
 
-fn generate_pair() -> (String, String) {
-    let sender = generate_username();
-    let mut recipient = generate_username();
+async fn set_username(user: &mut GooseUser) -> TransactionResult {
+    user.set_session_data(Username(generate_username()));
 
-    while sender == recipient {
-        recipient = generate_username();
-    }
-
-    (sender, recipient)
+    Ok(())
 }
 
 async fn post_message(user: &mut GooseUser) -> TransactionResult {
-    let (sender, recipient) = generate_pair();
+    let sender = &user.get_session_data::<Username>().unwrap().0;
+    let recipient = generate_username();
 
     let mut headers = HeaderMap::new();
-    let auth_name = "Bearer ".to_owned() + sender.as_str();
+    let auth_name = "Bearer ".to_owned() + &sender;
     headers.insert("Authorization", HeaderValue::from_str(&auth_name).unwrap());
     headers.insert(
         "Content-Type",
@@ -90,16 +98,11 @@ async fn post_message(user: &mut GooseUser) -> TransactionResult {
 }
 
 async fn delete_mailbox_content(user: &mut GooseUser) -> TransactionResult {
-    let username = generate_username();
+    let username = &user.get_session_data::<Username>().unwrap().0;
 
-    // keeping same structure as above altho not necessary since there's no payload
     let mut headers = HeaderMap::new();
     let auth_name = "Bearer ".to_owned() + username.as_str();
     headers.insert("Authorization", HeaderValue::from_str(&auth_name).unwrap());
-    headers.insert(
-        "Content-Type",
-        HeaderValue::from_str("application/json").unwrap(),
-    );
 
     let request_builder = user
         .get_request_builder(&GooseMethod::Delete, "/outbox")?
@@ -112,67 +115,6 @@ async fn delete_mailbox_content(user: &mut GooseUser) -> TransactionResult {
         .build();
 
     let _goose_metrics = user.request(goose_request).await;
-
-    Ok(())
-}
-
-async fn delete_post_delete(user: &mut GooseUser) -> TransactionResult {
-    let (username, friend) = generate_pair();
-
-    // keeping same structure as above altho not necessary since there's no payload
-    let mut headers = HeaderMap::new();
-    let auth_name = "Bearer ".to_owned() + username.as_str();
-    headers.insert("Authorization", HeaderValue::from_str(&auth_name).unwrap());
-    headers.insert(
-        "Content-Type",
-        HeaderValue::from_str("application/json").unwrap(),
-    );
-
-    // FIX: there has to be a better way to do this
-    let request_builder_delete1 = user
-        .get_request_builder(&GooseMethod::Delete, "/outbox")?
-        .headers(headers.clone());
-
-    let request_builder_delete2 = user
-        .get_request_builder(&GooseMethod::Delete, "/outbox")?
-        .headers(headers.clone());
-
-    let goose_request_delete1 = GooseRequest::builder()
-        .method(GooseMethod::Delete)
-        .path("/outbox")
-        .set_request_builder(request_builder_delete1)
-        .build();
-
-    let goose_request_delete2 = GooseRequest::builder()
-        .method(GooseMethod::Delete)
-        .path("/outbox")
-        .set_request_builder(request_builder_delete2)
-        .build();
-
-    let request_builder_post = user
-        .get_request_builder(&GooseMethod::Post, "/message")?
-        .headers(headers);
-
-    let data = json!(
-    {
-        "batch":
-        [
-        {"deviceId": username,
-        "payload": {"cType":0, "ciphertext": "hello"}},
-        { "deviceId": friend,
-        "payload": {"cType":0, "ciphertext": "goodbye"}},
-        ]
-    });
-
-    let goose_request_post = GooseRequest::builder()
-        .method(GooseMethod::Post)
-        .path("/message")
-        .set_request_builder(request_builder_post.json(&data))
-        .build();
-
-    let _goose_metrics = user.request(goose_request_delete1).await;
-    let _goose_metrics = user.request(goose_request_post).await;
-    let _goose_metrics = user.request(goose_request_delete2).await;
 
     Ok(())
 }
